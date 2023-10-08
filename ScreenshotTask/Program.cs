3159ba88a4aa5f3879692fe3ce07a4119f3590ec
Nuri <Nuri@DESKTOP-V9A4QK4>
2023-10-08 09:19:39 -0700

    Screenshot Task

---
+++ ScreenshotTask/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Drawing;
+using System.IO;
+
+
+
+while (true)
+{
+    ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+    if (keyInfo.Key == ConsoleKey.Enter)
+    {
+        Rectangle screenBounds = new Rectangle(0, 0, (int)SystemParameters.PrimaryScreenWidth, (int)SystemParameters.PrimaryScreenHeight);
+
+        
+        using (Bitmap bitmap = new Bitmap(screenBounds.Width, screenBounds.Height))
+        {
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                g.CopyFromScreen(screenBounds.Location, Point.Empty, screenBounds.Size);
+            }
+
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string fileName = $"screenshot_{DateTime.Now:yyyyMMdd_HHmmss}.png";
+            string filePath = Path.Combine(desktopPath, fileName);
+
+            bitmap.Save(filePath, ImageFormat.Png);
+        }
+    }
+}
+               
